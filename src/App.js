67770df784ca192f35b4fe545f1bb09ad3a10228
miyagin15 <miya15work@yahoo.co.jp>
2020-04-06 23:36:33 +0900

    計算されるように

--- src/App.js
+++ src/App.js
@@ -8,22 +8,20 @@ class App extends React.Component  {
     //必須
     super(props);
 
-  this.state ={textValue :"100",percent:"0.5"}
+  this.state ={weight :"100",percent:"0.5"}
   }
   changeText=(e)=> {
-    console.log(e.target.value)
     this.setState({[e.target.name]: e.target.value});
   }
   render(){
   return (
     <div>
-      <progress id="myProgress" value="40" max="100">0%</progress>
+      <progress id="myProgress" value={this.state.percent} max="10">0%</progress>
       <p>全部で何g？</p>
-        <input type="text" name="weight" value={this.state.textValue} onChange={this.changeText} />g
+        <input type="text" name="weight" value={this.state.weight} onChange={this.changeText} />g
         <p>塩分%は？</p>
         <input type="text" name="percent" value={this.state.percent} onChange={this.changeText} />%
-        <p>{this.state.textValue*this.state.percent}</p>
-        <p>{this.state.percent}</p>
+        <p>塩{this.state.weight*this.state.percent*0.01}g入れる</p>
         </div>
 
   );
